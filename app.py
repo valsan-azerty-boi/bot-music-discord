d41e89273f8d582a672b5c394e335c93bbec66d5
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2021-08-28 04:05:35 +0200

    update play command for better search argurments, update formatting

--- app.py
+++ app.py
@@ -25,18 +25,18 @@ bot = commands.Bot(command_prefix='-',intents=intents)
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
-    'format': 'bestaudio/best',
+	'format': 'bestaudio/best',
 	'default_search': 'auto',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': False,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'
+	'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+	'restrictfilenames': True,
+	'noplaylist': False,
+	'nocheckcertificate': True,
+	'ignoreerrors': False,
+	'logtostderr': False,
+	'quiet': True,
+	'no_warnings': True,
+	'default_search': 'auto',
+	'source_address': '0.0.0.0'
 }
 
 ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
@@ -44,20 +44,20 @@ ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconne
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ""
+	def __init__(self, source, *, data, volume=0.5):
+		super().__init__(source, volume)
+		self.data = data
+		self.title = data.get('title')
+		self.url = ""
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
+	@classmethod
+	async def from_url(cls, url, *, loop=None, stream=False):
+		loop = loop or asyncio.get_event_loop()
+		data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+		if 'entries' in data:
+			data = data['entries'][0]
+		filename = data['title'] if stream else ytdl.prepare_filename(data)
+		return filename
 
 @bot.command(name='join', help='Tells the bot to join a voice channel')
 async def join(ctx):
@@ -69,7 +69,7 @@ async def join(ctx):
 		await channel.connect()
 	except Exception:
 		pass
-    
+	
 @bot.command(name='leave', help='To make the bot leave the voice channel')
 async def leave(ctx):
 	try:
@@ -80,20 +80,20 @@ async def leave(ctx):
 		pass
 @bot.command(name='disconnect', help='To make the bot leave the voice channel')
 async def disconnect(ctx):
-    await leave(ctx)
+	await leave(ctx)
 
 @bot.command(name='p', help='To play a song')
 async def p(ctx,url):
-    await play(ctx,url)
+	await play(ctx,url)
 
 @bot.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
-    try:
-        voice_client = ctx.message.guild.voice_client
-        if voice_client.is_playing():
-            await voice_client.pause()
-    except Exception:
-        pass
+	try:
+		voice_client = ctx.message.guild.voice_client
+		if voice_client.is_playing():
+			await voice_client.pause()
+	except Exception:
+		pass
 		
 @bot.command(name='resume', help='Resumes the song')
 async def resume(ctx):
@@ -106,7 +106,7 @@ async def resume(ctx):
 		
 @bot.command(name='unpause', help='Resumes the song')
 async def unpause(ctx):
-    await resume(ctx)
+	await resume(ctx)
 
 @bot.command(name='stop', help='Stops the song')
 async def stop(ctx):
@@ -118,7 +118,8 @@ async def stop(ctx):
 		pass
 
 @bot.command(name='play', help='To play song')
-async def play(ctx,url):
+async def play(ctx, *args):
+	await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 	await stop(ctx)
 	await join(ctx)
 	try:
@@ -126,7 +127,7 @@ async def play(ctx,url):
 			server = ctx.message.guild
 			voice_channel = server.voice_client
 			with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
-				info = ydl.extract_info(url, download=False)
+				info = ydl.extract_info("", download=False)
 			if 'entries' in info:
 				vid = info['entries'][0]["formats"][0]
 			elif 'formats' in info:
@@ -135,6 +136,10 @@ async def play(ctx,url):
 		await ctx.send("Now playing the requested song.")
 	except Exception:
 		pass
+		
+@bot.command(name='test', help='To play a test')
+async def test(ctx, *args):
+	await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 	
 @bot.command(name='bob', help='Hey it`s me')
 async def bob(ctx):
@@ -155,4 +160,3 @@ if __name__ == "__main__" :
 	bot.run(DISCORD_TOKEN)
 
 # TODO add a queue system for songs
-# TODO enhance play command for search args
