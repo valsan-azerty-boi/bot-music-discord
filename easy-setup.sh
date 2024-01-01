ed69423a6ada9ebe4d2b091cb33cd25058afe7aa
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2024-01-01 20:21:18 +0100

    update easy-setup.sh

--- easy-setup.sh
+++ easy-setup.sh
@@ -13,3 +13,8 @@ docker build -t bot-music-discord .
 #docker run -d --name bot-music-discord -P bot-music-discord
 #docker run -d --name bot-music-discord --restart always -P bot-music-discord
 docker run -d --name bot-music-discord --restart always -P --cpus 2.0 --memory 2g --memory-swap 2g bot-music-discord
+
+# Before running this script (first time only): 
+# Make script executable: chmod +x ./easy-setup.sh
+# Add to crontab if needed: 0 5 * * * /bin/bash /path/to/easy-setup.sh
+# Add to crontab if needed: @reboot /bin/bash /path/to/easy-setup.sh
