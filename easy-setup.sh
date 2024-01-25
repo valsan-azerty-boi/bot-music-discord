55f908e62ae2090f81622265adc13b7bda094b12
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2024-01-25 15:20:59 +0100

    update script

--- easy-setup.sh
+++ easy-setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Stop and remove the existing container
 docker stop bot-music-discord
 docker rm bot-music-discord
 
 # Change to the bot's directory and pull the latest changes
-cd /home/discordbot/bot-music-discord #edit your path
+cd "$SCRIPT_DIR"
 git pull origin master
 
 # Build and run the Docker container
 docker build -t bot-music-discord .
-#docker run -d --name bot-music-discord -P bot-music-discord
-#docker run -d --name bot-music-discord --restart always -P bot-music-discord
 docker run -d --name bot-music-discord --restart always -P --cpus 2.0 --memory 2g --memory-swap 2g bot-music-discord
 
 # Before running this script (first time only): 
